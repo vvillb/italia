0c81c568fe4d413226f4a54f0f47a845218be4c5
vvillb <virginiavt98@gmail.com>
2022-11-09 11:38:23 +0100

    new date

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-var countDownDate= new Date("Dec 5, 2022, 20:00:00").getTime()
+var countDownDate= new Date("Dec 5, 2022, 10:00:00").getTime()
 var x =setInterval(function(){
     var now=new Date().getTime();
     var distance=countDownDate-now;
